0fb101a4c27cab793550e0b2b108134026246f9a
victor-hermogenes <victorltrgh@gmail.com>
2025-02-11 13:51:04 -0300

    Configurações básicas de database

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost/disconcord")
+
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
