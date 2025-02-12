da974935a5514d667ad48450c06550cc1de3d3ed
victor-hermogenes <victorltrgh@gmail.com>
2025-02-11 22:14:59 -0300

    Fixes na database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,22 +1,32 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenvPath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),"scripts/.env")
+load_dotenv(dotenvPath, override=True)
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost/disconcord")
+DATA_BASE_ASYNC  = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+if not DATA_BASE_ASYNC:
+    raise ValueError("DATABASE_URL não encontrado no .env, cheque o arquivo.")
 
-AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
+print("DATABASE_URL carregado: ", DATA_BASE_ASYNC)
+
+DATA_BASE_SYNC = DATA_BASE_ASYNC.replace("+asyncpg", "") if DATA_BASE_ASYNC else None
+
+if not DATA_BASE_SYNC:
+    raise ValueError("DATABASE_URL_SYNC é invalido! Cheque suas configurações do .env.")
+
+engine_async = create_async_engine(DATA_BASE_ASYNC, future=True, echo=True)
+engine_sync = create_engine(DATA_BASE_SYNC, future=True, echo=True)
+
+AsyncSessionLocal = sessionmaker(bind=engine_async, class_=AsyncSession, expire_on_commit=False)
+SessionLocal = sessionmaker(bind=engine_sync, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 
 async def get_db():
-    async with AsyncSessionLocal() as session:
+    async with AsyncSessionLocal as session:
         yield session
