6e702398b487ca2796c41bba619c35f553932541
victor-hermogenes <victorltrgh@gmail.com>
2025-02-12 21:36:54 -0300

    service para lidar com os dados do disconcord

---
+++ backend/app/services/auth_service.py
@@ -0,0 +1,25 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
