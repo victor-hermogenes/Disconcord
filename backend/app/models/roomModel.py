4e356de7154632ecf848c6664744a891cc0e65d7
victor-hermogenes <victorltrgh@gmail.com>
2025-02-12 21:36:37 -0300

    Mudança de nomes e ajuste de models

--- backend/app/models/roomModel.py
+++ backend/app/models/roomModel.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func 
-from backend.app.database import Base 
+from backend.app.database import Base
 
 class Room(Base):
     __tablename__ = "rooms"
--- backend/app/models/userModel.py
+++ backend/app/models/userModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from backend.app.database import Base
@@ -12,4 +12,4 @@ class User(Base):
     password_hash = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
 
-    rooms = relationship("Room", back_populates="owner", cascade="all, delete-orphan")
+    rooms = relationship("Room", back_populates="owner", cascade="all, delete-orphan", lazy="joined")
