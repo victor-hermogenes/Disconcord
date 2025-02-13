474c6ad530c5cb3d4a3c5117f253649ede98ae49
victor-hermogenes <victorltrgh@gmail.com>
2025-02-12 23:59:47 -0300

    Incluindo rotas de usuário

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from backend.app.routes.authRoutes import router as auth_router
+from backend.app.routes.usersRoutes import router as users_router
 
 app = FastAPI(title="Disconcord API", version="1.0.0", description="A Discord Clone API")
 
 app.include_router(auth_router)
+app.include_router(users_router)
 
 @app.get("/")
 def home():
