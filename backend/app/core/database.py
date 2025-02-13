18e69c1ab97c4a03fe673ab6ab6980560d3e7f65
victor-hermogenes <victorltrgh@gmail.com>
2025-02-13 19:34:33 -0300

    Update da database para mais estrutura

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,20 +4,20 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
-dotenvPath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))),".env")
+dotenvPath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), ".env")
 load_dotenv(dotenvPath, override=True)
 
-DATA_BASE_ASYNC  = os.getenv("DATABASE_URL")
+DATA_BASE_ASYNC = os.getenv("DATABASE_URL")
 
 if not DATA_BASE_ASYNC:
     raise ValueError("DATABASE_URL não encontrado no .env, cheque o arquivo.")
 
-print("DATABASE_URL carregado: ", DATA_BASE_ASYNC)
+print("DATABASE_URL carregado:", DATA_BASE_ASYNC)
 
 DATA_BASE_SYNC = DATA_BASE_ASYNC.replace("+asyncpg", "") if DATA_BASE_ASYNC else None
 
 if not DATA_BASE_SYNC:
-    raise ValueError("DATABASE_URL_SYNC é invalido! Cheque suas configurações do .env.")
+    raise ValueError("DATABASE_URL_SYNC é inválido! Cheque suas configurações do .env.")
 
 engine_async = create_async_engine(DATA_BASE_ASYNC, future=True, echo=True)
 engine_sync = create_engine(DATA_BASE_SYNC, future=True, echo=True)
@@ -27,8 +27,7 @@ SessionLocal = sessionmaker(bind=engine_sync, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 
-import backend.app.models.userModels
-import backend.app.models.roomModels
+from backend.app.models import userModels, roomModels, chatModels
 
 async def get_db():
     async with AsyncSessionLocal() as session:
