db091e165452717a53530750aeea959ca4a379bb
victor-hermogenes <victorltrgh@gmail.com>
2025-02-13 21:48:44 -0300

    Dependencias reestruturada

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,12 +1,11 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
-from backend.app.services.authService import SECRET_KEY, ALGORITHM
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from backend.app.core.database import SessionLocal
 from backend.app.models.userModels import User
+from backend.app.services.authService import decode_access_token
+from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_db():
     db = SessionLocal()
@@ -15,22 +14,14 @@ def get_db():
     finally:
         db.close()
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Não foi possível validar as credenciais.",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Token inválido ou expirado")
 
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-
+    username = payload.get("sub")
     user = db.query(User).filter(User.username == username).first()
-    if user is None:
-        raise credentials_exception
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
     return user
